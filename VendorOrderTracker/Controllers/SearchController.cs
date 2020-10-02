3041b1cce86929948589d9adce7b74c3b9c232d3
William Donovan-Seid <wdstwo@live.com>
2020-10-02 14:25:31 -0700

    add basic search for Vendors Controller, and Views

---
+++ VendorOrderTracker/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class SearchController : Controller
+  {
+
+    [HttpGet("/search")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet("/search/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/search")]
+    public ActionResult Create(string inputVendorName)
+    {
+      List<Vendor> matchingVendors = new List<Vendor>{};
+      List<Vendor> allVendors = Vendor.GetAllVendors();
+      foreach (Vendor vendor in allVendors)
+      {
+        if (vendor.Name.ToUpper() == inputVendorName.ToUpper() || vendor.Name.Contains(inputVendorName))
+        {
+          matchingVendors.Add(vendor);
+        }
+      }
+      return View("Index", matchingVendors);
+    }
+  }
+}

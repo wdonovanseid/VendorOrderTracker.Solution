811995f30bbad1e799fcf8d642a8957ce72fc8ec
William Donovan-Seid <wdstwo@live.com>
2020-10-02 08:44:46 -0700

    add Index.cshtml in Home Views and basic _Layout.cshtml in Shared Views

---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  
+}
---
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAllVendors();
+      return View(allVendors);
+    }
+
+  }
+}
